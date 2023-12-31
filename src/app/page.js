9be0aa60b11520461d5b21e2d913cee758f7e8e0
dolloct20 <dolloct20@gmail.com>
2023-12-31 20:21:39 +0800

    feat: added mock product data to the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,126 @@
+"use client"
 import Image from 'next/image'
 
 export default function Home() {
+  const productList = [
+      {
+          id: '000001',
+          type: 'Prebuilt PC',
+          name: 'Creator RDY Y40BG201',
+          cpu: 'Intel® Core™ i9-13900KF CPU',
+          os: 'Windows 11 Pro',
+          memory: '32GB DDR5-6000MHz RGB RAM',
+          gpu: 'GeForce RTX 4090 - 24GB',
+          storage: '2TB M.2 NVMe GEN4 SSD',
+          image: ['https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26779/gaming-pc-01-SLMBG221-KBM-400.png?v=822a5a78181bcb5d049c8632416573af863cdf60'],
+          affirmMonthlyPay: 160,
+          originalPrice: 3799,
+          salePrice: 3299
+      },
+      {
+          id: '000002',
+          type: 'Prebuilt PC',
+          name: 'Gaming RDY SLMBG221',
+          cpu: 'Intel® Core™ i7-13700F CPU',
+          os: 'Windows 11 Home',
+          memory: '16GB DDR5-5200MHz RAM',
+          gpu: 'GeForce RTX 4060 Ti - 8GB',
+          storage: '1TB M.2 NVMe SSD',
+          image: ['https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26779/gaming-pc-01-SLMBG221-KBM-400.png?v=822a5a78181bcb5d049c8632416573af863cdf60'],
+          affirmMonthlyPay: 81,
+          originalPrice: 1599,
+          salePrice: 1299
+      },
+      {
+          id: '000003',
+          type: 'Custom PC',
+          name: 'AMD Gaming PC Configurator 4 ',
+          cpu: 'AMD Ryzen™ 9 7900X CPU',
+          os: 'Windows 11 Home',
+          memory: '32GB DDR5-5200MHz RAM',
+          gpu: 'NVIDIA GeForce RTX 4070 Ti - 12GB',
+          storage: '1TB WD M.2 NVMe SSD',
+          image: ['https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/25756/gaming-pc-01-H7-Flow-white-main-400.png?v=822a5a78181bcb5d049c8632416573af863cdf60'],
+          affirmMonthlyPay: 111,
+          originalPrice: 2589,
+          salePrice: 2289
+      },
+      {
+          id: '000004',
+          type: 'Custom PC',
+          name: 'AMD Gaming PC Configurator 3',
+          cpu: 'AMD Ryzen™ 7 7700X CPU',
+          os: 'Windows 11 Home',
+          memory: '32GB DDR5-5600MHz TEAMGROUP RAM',
+          gpu: 'NVIDIA GeForce RTX 4070 - 12GB',
+          storage: '1TB WD M.2 NVMe SSD',
+          image: ['https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26900/gaming-pc-01-Lancool-216-white-main-400,,,.png?v=64cb5f559abaf27f57b17b6087ecfb9547b6fae5'],
+          affirmMonthlyPay: 86,
+          originalPrice: 1969,
+          salePrice: 1769
+      },
+      {
+          id: '000005',
+          type: 'Custom PC',
+          name: 'Intel Core 14th Gen Extreme Gaming PC',
+          cpu: 'Intel® Core™ i9-14900KF CPU',
+          os: 'Windows 11 Home',
+          memory: '32GB DDR5-6000MHz TEAMGROUP RAM',
+          gpu: 'NVIDIA GeForce RTX 4090 - 24GB',
+          storage: '2TB WD M.2 NVMe SSD',
+          image: ['https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26936/gaming-pc-01-Lancool-216-black-main-400.png?v=822a5a78181bcb5d049c8632416573af863cdf60'],
+          affirmMonthlyPay: 177,
+          originalPrice: 3949,
+          salePrice: 3649
+      },
+      {
+          id: '000006',
+          type: 'Prebuilt PC',
+          name: 'RDY Fractal BG001',
+          cpu: 'Intel® Core™ i7-13700KF CPU',
+          os: 'Windows 11 Home',
+          memory: '32GB DDR5-6000MHz RAM',
+          gpu: 'GeForce RTX 4070 - 12GB',
+          storage: '2TB M.2 NVMe SSD',
+          image: ['https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/26934/gaming-pc-01-FractalBG001-main-400.png?v=64cb5f559abaf27f57b17b6087ecfb9547b6fae5'],
+          affirmMonthlyPay: 87,
+          originalPrice: 2149,
+          salePrice: 1799
+      },
+      {
+          id: '000007',
+          type: 'Custom PC',
+          name: 'Intel Gaming PC Configurator 2',
+          cpu: 'Intel® Core™ i5-13600KF CPU',
+          os: 'Windows 11 Home',
+          memory: '32GB DDR5-6000MHz TEAMGROUP RAM',
+          gpu: 'NVIDIA GeForce RTX 4060 Ti - 8GB',
+          storage: '1TB WD M.2 NVMe SSD',
+          image: ['https://content.ibuypower.com/cdn-cgi/image/width=256,format=auto,quality=75/https://content.ibuypower.com/Images/Components/25756/gaming-pc-01-H7-Flow-white-main-400.png?v=822a5a78181bcb5d049c8632416573af863cdf60'],
+          affirmMonthlyPay: 97,
+          originalPrice: 1709,
+          salePrice: 1559
+      },
+      {
+          id: '000008',
+          type: 'Custom PC',
+          name: 'Intel Gaming PC Configurator 4',
+          cpu: 'Intel® Core™ i9-13900KF CPU',
+          os: 'Windows 11 Home',
+          memory: '32GB DDR5-6000MHz TEAMGROUP RAM',
+          gpu: 'NVIDIA GeForce RTX 4070 Ti - 12GB',
+          storage: '1TB WD M.2 NVMe SSD',
+          image: ['https://content.ibuypower.com/cdn-cgi/image/width=640,format=auto,quality=75/https://content.ibuypower.com/Images/Components/25755/gaming-pc-01-H7-Flow-Black-main-400.png?v=64cb5f559abaf27f57b17b6087ecfb9547b6fae5'],
+          affirmMonthlyPay: 114,
+          originalPrice: 2649,
+          salePrice: 2349
+      }
+  ]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white text-gray-900">
+      <div className="container flex flex-col items-center justify-center">
+          <p className="text-3xl font-bold mb-2">Best Seller Gaming PC</p>
+          <p className="text-xl font-semibold mb-2">Prebuild & Customs</p>
       </div>
     </main>
   )
